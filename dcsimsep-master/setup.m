b32702f716f3a18f4a1c3c0b19d7bb4a459b0e85
AminMoradiXL <114115596+AminMoradiXL@users.noreply.github.com>
2022-09-21 16:07:04 -0700

    Add files via upload

---
+++ dcsimsep-master/setup.m
@@ -0,0 +1,8 @@
+
+% just set up the path so that we can use dcsimsep
+p = pwd;
+addpath([p '/dcsimsep-master/src']) % add the source directory
+addpath([p '/dcsimsep-master/cplex']);
+addpath([p '/dcsimsep-master/test']);
+addpath([p '/dcsimsep-master/data']);
+cd 
